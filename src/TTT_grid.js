95574537134c74fa20272ce4b3a24454b8174ff6
samtru99 <samtrujillo1999@gmail.com>
2023-11-22 11:45:33 -0800

    whole redesign from ground up - supports playing against the computer through random moves

---
+++ src/TTT_grid.js
@@ -0,0 +1,175 @@
+import {React, useState, useRef, useEffect} from 'react';
+import "./gameOver"
+import isWinner from './gameOver';
+import catScratch from './catScratch';
+
+const initGame = () => Array.from({ length: 9 }, (_, i) => ({
+  position: i,
+}))
+
+function TTT() 
+{
+  const [playersMove, setPlayersMove] = useState(true)
+  const [board, setBoard] = useState(initGame)
+  const [game, setGame] = useState(false)
+  const [tie, setTie] = useState(false)
+
+  const [firstUpdate, setFirstUpdate] = useState(true)
+  const [didOWin, setDidOWin] = useState(false)
+
+  const handlePlayersTurn = position => 
+  {
+    if(game)
+    {
+      return
+    }
+    // compute the symbol when you need it
+    const value = playersMove ? 'X' : 'O'
+    
+    // update the board using the previous board state
+    setBoard(b => b.toSpliced(position, 1, {
+      position,
+      value
+    }))
+    // update the current player
+
+    if(isWinner(board,playersMove))
+    {
+       setGame(true)
+       if(playersMove)
+       {
+        setDidOWin(true)
+       }
+       return
+    }
+
+    setPlayersMove(!playersMove)
+
+    
+  }
+  
+  // compute available positions
+  const availablePositions = board.filter(c => !c.value)
+    
+  useEffect(() => {
+
+    //1st render or someone won
+    if (firstUpdate)
+    {
+      setFirstUpdate(false)
+      return
+    }
+     //Check did O win
+     if(isWinner(board, true))
+     {
+       console.log("o won ")
+       setGame(true)
+       setDidOWin(true)
+       return
+     }
+    //Return if players X
+    if(playersMove)
+    {
+      return
+    }
+    //Board is full - determine if win or tie
+    if(availablePositions.length === 0)
+    {
+      if(isWinner(board, false))
+      {
+        setGame(true)
+        return
+      }
+      setTie(true)
+      return
+    }
+    
+    // if it's the player's turn or no more positions, skip
+    // find an open position
+    const idx = Math.floor(Math.random() * availablePositions.length)
+
+    // call handlePlayersTurn with open position found
+    handlePlayersTurn(availablePositions[idx].position)
+    //setGame(isWinner(board, true))
+    //setDidOWin(isWinner(board, true))
+
+  }, [playersMove, availablePositions,firstUpdate,board])
+  
+
+  return (
+    <div>
+    {tie && game != true? (
+       <h1 style={outcomeText}> Tie game </h1>
+    ) : (
+      <>
+      {game ? (
+          <>
+          {didOWin ? (<h1 style={outcomeText}> you lost </h1>):(
+            <h1 style={outcomeText}> you won </h1>
+          )}
+          </>
+        ) : (
+           <div style={boardStyle}>
+            {board.map(({ position, value }) => (
+              <div
+                  {...!value && { onClick: () => handlePlayersTurn(position) }}
+                  style={cell}
+                  key={position}>
+                    
+                  {value || '-'}
+              </div>
+            ))}
+            </div>      
+          )}
+      </>
+    )}
+  </div>
+);
+
+  /*
+  return (    
+    <div style={boardStyle}>
+      {board.map(({ position, value }) => (
+        <div
+          {...!value && { onClick: () => handlePlayersTurn(position) }}
+          style={cell}
+          key={position}>
+          {value || '-'}
+        </div>
+      ))}
+    </div>
+  )
+  */
+};
+
+const cell = {
+    display: 'flex',
+    border: '1px solid black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+
+const boardStyle = {
+display: 'grid',
+height: '500px',
+width: '500px',
+gridTemplateRows: 'repeat(3, 1fr)',
+gridTemplateColumns: 'repeat(3, 1fr)',
+};
+  
+const bigSquareStyle = {
+  width: '1000px',
+  height: '1000px', 
+  backgroundColor: 'grey', 
+  position: 'absolute',
+};
+
+const outcomeText = {
+  position: 'absolute',
+  top: '30%',
+  left: '40%'
+
+}
+
+export default TTT;
